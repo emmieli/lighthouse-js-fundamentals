f53ed138d0f6ebe3612bdd3794c4fafde2597542
Emmie Li <emmieli.bus@gmail.com>
2018-02-28 02:47:17 +0000

    Adds Functions Exercises

---
+++ hello.js
@@ -0,0 +1,10 @@
+function sayHello () {
+  console.log('Hello, world');
+}
+sayHello();
+function sayHello(name) {
+  csonsole.log('Hello, ' + name);
+}
+sayHello('Caliban');
+sayHello('Maranda');
+sayHello('Ferdinand');
